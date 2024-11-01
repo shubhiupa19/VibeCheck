ee932751cfe894f75641032fec68c80b1ea41a2b
shubhiupa19 <s.x.upadhyay@gmail.com>
2024-11-01 14:52:40 -0400

    added basic mongoose setup

--- app.mjs
+++ app.mjs
@@ -1,9 +1,13 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// will add the connection string later
+mongoose.connect();
 
 app.listen(process.env.PORT || 3000);
