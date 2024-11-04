f6736e1e7cedb4d867e036ce6c62a84b6cf21f7a
shubhiupa19 <s.x.upadhyay@gmail.com>
2024-11-03 23:01:24 -0500

    added one form, connected it to backend

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,13 +1,71 @@
+/**
+ * Get the filename and directory name of the current module.
+ * 
+ * `__filename` is the full path of the current module file.
+ * `__dirname` is the directory name of the current module file.
+ */
+import './config.mjs'
 import express from 'express'
+import session from 'express-session';
 import path from 'path'
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
+import './db.mjs';
+import cors from 'cors';
 
+
+
+
+// create a new express application
 const app = express();
+
+// using cors bc the frontend and backend are on different ports
+app.use(cors());
+
+
+// middleware to parse JSON bodies
+app.use(express.json());
+
+// getting the filename and directory name of the current module, 
+// so we can use them to serve static files
+// not sure if i need to use this yet, since the frontend will be in react
+// and will thus render the html and css files
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// will add the connection string later
-mongoose.connect();
+const Friend = mongoose.model('Friend');
+const User = mongoose.model('User');
 
-app.listen(process.env.PORT || 3000);
+
+// middleware that serves static file
+app.use(express.static(path.join(__dirname, 'public')));
+// middlware that parses url-encoded bodies coming in through the request
+app.use(express.urlencoded({ extended: false }));
+// middleware that keeps track of sessions
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.post('/friends', async (req, res) => {
+    // console.log("body", req.body);
+    const { name, about } = req.body;
+    console.log("about", about);
+    try {
+        const friend = new Friend ({ name, about});
+        await friend.save();
+        
+        res.status(201).json(friend);
+
+    } catch(error) {
+        console.log("error saving friend")
+        res.status(500).json( { message: error.message });
+    }
+})
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running... on port', process.env.PORT);
+});
--- backend/db.mjs
+++ backend/db.mjs
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
+mongoose.connect(process.env.DSN)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(error => console.error(error));
+
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true},
@@ -9,7 +13,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 const FriendSchema = new mongoose.Schema({ 
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    // user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     name: { type: String, required: true},
     about: [ { type: String }],
     interactions: [{ 
@@ -21,5 +25,9 @@ const FriendSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
+// registering the models
 mongoose.model('User', UserSchema);
 mongoose.model('Friend', FriendSchema);
+
+// export the models
+
